053134fff817c01608bfdbcd135fd720624274f7
Dhruv Bana <114687171+banadhruva@users.noreply.github.com>
2026-01-24 12:47:45 +0530

    Create 50. Pow(x, n).py

---
+++ 50. Pow(x, n).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        x = float(x)
+        n = int(n)
+        return pow(x, n)
